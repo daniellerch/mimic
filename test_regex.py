1d2be7e3f31379061e89fa6bdb4371dfab4d2cf5
Daniel Lerch <dlerch@gmail.com>
2016-07-17 23:13:34 +0200

    test regex

---
+++ test_regex.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+import glob
+import re
+
+if __name__ == '__main__':
+
+   files=glob.glob("learn/*.text")
+
+   for fn in files:
+      print "Reading", fn
+      with open(fn) as f:
+         lines = f.readlines()
+
+      for l in lines:
+         l=l[:-1]+"."
+         print l
+         m = re.search('(\w+) (\w+) (es|son) (\w+) (\w+).', l)
+         if m:
+            print "grp: ", m.group(2), ":", m.group(5)
+         print "--"
+
+
+
+
