0f53385bb73db12143ebf27d183e889c4cf93ee4
Daniel Lerch <dlerch@gmail.com>
2016-07-19 18:39:13 +0200

    Revert "Word class"
    This reverts commit ddcb5fe9bcdf1919c678a19f17aa992cfd022eae.

--- language/es/words.py
+++ language/es/words.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-# Copyright (c) 2011-2013 University of Antwerp, Belgium
-
-from pattern.es import singularize, pluralize, attributive, predicative
-from pattern.es import MALE, FEMALE
+from unidecode import unidecode
 
 definite_articles=["el", "la", "los", "las"]
 indefinite_articles=["un", "uno", "una", "unos", "unas"]
@@ -13,66 +9,120 @@ indefinite_articles=["un", "uno", "una", "unos", "unas"]
 is_a_relation=["es", "son"]
 
 class Word():
-
+   
    # {{{ __init__()
    def __init__(self, w):
+      self.is_masculine=True
+      self.is_singular=True
       self.word = w.lower()
-      self.is_singular=True
-      self.is_masculine=True
-      self.to_masculine()
       self.to_singular()
+      self.to_masculine()
    # }}}
 
-   # {{{ to_singular()
-   def to_singular(self):
-      self.word = singularize(self.word)
-      self.is_singular=True
-      return self
-   # }}}
+# {{{ to_singular()
+def to_singular(self):
 
-   # {{{ to_plural()
-   def to_plural(self):
-      self.word = pluralize(self.word)
-      self.is_singular=False
-      return self
-   # }}}
+   self.is_singular=True
+   w=self.f
 
-   # {{{ to_feminine()
-   def to_feminine(self):
-      self.word = attributive(self.word, gender=FEMALE)
+   if w.endswith("es") and w[:-2].endswith(("br", "i", "j", "t", "zn")):
+      return w[:-1]
 
-      if self.is_singular:
-         self.to_singular()
-      else:
-         self.to_plural()
+   for s, p in ( ("anes", u"án"), ("enes", u"én"), ("eses", u"és"),
+                 ("ines", u"ín"), ("ones", u"ón"), ("unes", u"ún") ):
+      if w.endswith(s):
+         return w[:-4] + p
 
-      self.is_masculine=False
-      return self
-   # }}}
+   if w.endswith(("esis", "isis", "osis")):
+      return w
 
-   # {{{ to_masculine()
-   def to_masculine(self):
-      self.word = predicative(self.word)
+   if w.endswith("ces"):
+      return w[:-3] + "z"
 
-      if self.is_singular:
-         self.to_singular()
-      else:
-         self.to_plural()
+   if w.endswith("es"):
+      return w[:-2]
 
-      self.is_masculine=True
-      return self
-   # }}}
+   if w.endswith("s"):
+      return w[:-1]
+
+   return w
+# }}}
+
+# {{{ to_plural()
+def to_plural(self):
+
+   self.is_singular=False
+   w = self.w
+
+   for a, b in ( (u"mamá", u"mamás"), (u"papá", u"papás"),
+                 (u"sofá", u"sofás") , (u"dominó", u"dominós") ):
+      if w==a:
+         return b
 
 
-# Tes
-if __name__ == '__main__':
+   if w.endswith(("idad", "esis", "isis", "osis", "dica", u"grafía", u"logía")):
+      return w
 
-   for string in ["perro", "mascotas", u"mamás", "pueril", "Juan", "Juana"]:
-      w = Word(string)
-      print w.to_masculine().to_singular().word
-      print w.to_masculine().to_plural().word
-      print w.to_feminine().to_singular().word
-      print w.to_feminine().to_plural().word
-      print "--"
+   if w.endswith( ("a", "e", "i", "o", "u") ) or w.endswith(u"é"):
+      return w + "s"
+
+   if w.endswith((u"á", u"é", u"í", u"ó", u"ú")):
+      return w + "es"
+
+   if w.endswith(u"és"):
+      return w[:-2] + "eses"
+
+   if w.endswith(u"s") and len(w) > 3 and is_vowel(w[-2]):
+      return w
+
+   if w.endswith(u"z"):
+      return w[:-1] + "ces"
+
+   for a, b in ( (u"án", "anes"), (u"én", "enes"), (u"ín", "ines"),
+                 (u"ón", "ones"), (u"ún", "unes") ):
+      if w.endswith(a):
+         return w[:-2] + b
+
+   return w + "es"
+# }}}
+
+# {{{ to_feminine()
+def to_feminine(self):
+
+   self.is_masculine=False
+   w = self.w
+
+    w = adjective.lower()
+    # normal => normales
+    if PLURAL in gender and not is_vowel(w[-1:]):
+        return w + "es" 
+    # el chico inteligente => los chicos inteligentes
+    if PLURAL in gender and w.endswith(("a", "e")):
+        return w + "s"
+    # el chico alto => los chicos altos
+    if w.endswith("o"):
+        if FEMININE in gender and PLURAL in gender:
+            return w[:-1] + "as"
+        if FEMININE in gender:
+            return w[:-1] + "a"
+        if PLURAL in gender:
+            return w + "s"
+ 
+
+# }}}
+
+# {{{ to_masculine()
+def to_masculine(self):
+
+   self.is_masculine=True
+   w = self.w
+
+   if w.endswith("as"):
+       w = w[:-2]+"os"
+
+   if w.endswith("a"):
+       return w[:-1] + "o"
+
+# }}}
 
 
